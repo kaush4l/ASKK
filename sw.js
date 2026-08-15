aa8df02edbbe7265037196f865b58d74d9f46558
kaush <kaushal.beyblader@gmail.com>
2026-08-15 01:41:40 -0400

    deploy ec86ff3

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // worker that can serve a shell from a build whose assets are gone.
 importScripts("coi-sw.js");
 
-const VERSION = "96030b3";
+const VERSION = "ec86ff3";
 const CACHE = "askk-" + VERSION;
 
 // The container2wasm runtime is ~47MB of bytes that DO NOT CHANGE between
