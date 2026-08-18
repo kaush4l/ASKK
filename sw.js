81d2826cbcbfa546e28965945aa8363dd7b1a973
kaush <kaushal.beyblader@gmail.com>
2026-08-18 01:19:06 -0400

    deploy 187dc39

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 // worker that can serve a shell from a build whose assets are gone.
 importScripts("coi-sw.js");
 
-const VERSION = "426146d";
+const VERSION = "187dc39";
 const CACHE = "askk-" + VERSION;
 
 // The container2wasm runtime is ~47MB of bytes that DO NOT CHANGE between
